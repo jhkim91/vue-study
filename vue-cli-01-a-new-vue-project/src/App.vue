1f43795931931cdf4e0bc0c753ae824ffe9639fd
kimjunhee <developer9101@gmail.com>
2023-04-04 10:46:34 +0900

    props 양방향 통신

--- vue-cli-01-a-new-vue-project/src/App.vue
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -7,10 +7,12 @@
       <friend-contact
         v-for="friend in friends"
         :key="friend.id"
+        :id="friend.id"
         :name="friend.name"
         :phone-number="friend.phone"
         :email-address="friend.email"
         :is-favorite="friend.isFavorite"
+        @toggle-favorite="toggleFavoriteStatus"
       />
     </ul>
   </section>
@@ -39,6 +41,14 @@ export default {
         },
       ],
     };
+  },
+  methods: {
+    toggleFavoriteStatus(friendId) {
+      const identifiedFriend = this.friends.find(
+        (friend) => friend.id === friendId
+      );
+      identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+    },
   },
 };
 </script>
