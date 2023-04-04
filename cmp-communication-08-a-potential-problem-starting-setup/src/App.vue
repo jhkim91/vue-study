196e00a1fc503c3dff01b9f070ff36fc2ef1ec82
kimjunhee <developer9101@gmail.com>
2023-04-04 12:03:50 +0900

    provide, inject 사용해서 function 전달

--- cmp-communication-08-a-potential-problem-starting-setup/src/App.vue
+++ cmp-communication-08-a-potential-problem-starting-setup/src/App.vue
@@ -4,7 +4,7 @@
       :topic-title="activeTopic && activeTopic.title"
       :text="activeTopic && activeTopic.fullText"
     ></active-element>
-    <knowledge-base @select-topic="activateTopic"></knowledge-base>
+    <knowledge-base></knowledge-base>
   </div>
 </template>
 
@@ -34,7 +34,7 @@ export default {
   },
   // provide: { topics: [] }, // object로 넘길경우 동기화안됨
   provide() {
-    return { topics: this.topics };
+    return { topics: this.topics, selectTopic: this.activateTopic };
   },
   methods: {
     activateTopic(topicId) {
