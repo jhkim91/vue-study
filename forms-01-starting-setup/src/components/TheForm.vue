4778a08aff43662a88eaabacf50962cc79ca28a7
kimjunhee <developer9101@gmail.com>
2023-04-05 16:12:43 +0900

    valid 체크(@blur 사용)

--- forms-01-starting-setup/src/components/TheForm.vue
+++ forms-01-starting-setup/src/components/TheForm.vue
@@ -1,8 +1,18 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div
+      class="form-control"
+      :class="{ invalid: userNameValidity === 'invalid' }"
+    >
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="userName" />
+      <input
+        id="user-name"
+        name="user-name"
+        type="text"
+        v-model.trim="userName"
+        @blur="validateInput"
+      />
+      <p v-if="userNameValidity === 'invalid'">Please enter a valid name!</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -108,6 +118,7 @@ export default {
       interest: [],
       how: null,
       confirm: false,
+      userNameValidity: 'pending',
     };
   },
   methods: {
@@ -117,6 +128,13 @@ export default {
       console.log('referrer', this.referrer);
       console.log('interest', this.interest);
       console.log('how', this.how);
+    },
+    validateInput() {
+      if (this.userName === '') {
+        this.userNameValidity = 'invalid';
+      } else {
+        this.userNameValidity = 'valid';
+      }
     },
   },
 };
@@ -134,6 +152,14 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+
+.form-control.invalid input {
+  border-color: red;
+}
+
+.form-control.invalid label {
+  color: red;
 }
 
 label {
