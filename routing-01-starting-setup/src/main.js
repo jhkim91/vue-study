b84876b01eb1c69cb0de041e7a185a91e62e8273
kimjunhee <developer9101@gmail.com>
2023-04-05 22:49:15 +0900

    ★라우트 설정 및 사용

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -1,7 +1,19 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue'
+import UsersList from './components/users/UsersList.vue'
 
-const app = createApp(App)
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
+    { path: '/users', component: UsersList },
+  ],
+  // linkActiveClass: 'active' // active class name 변경
+});
+const app = createApp(App);
+app.use(router); // 서드 파티 패키지와 다른 기능을 연결해주는 역할
 
 app.mount('#app');
