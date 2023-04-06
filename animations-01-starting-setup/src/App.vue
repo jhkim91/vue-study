102fdf860d2815ad1148157a82ae951e45ccbf92
kimjunhee <developer9101@gmail.com>
2023-04-06 14:50:22 +0900

    transition 태그에 name 지정

--- animations-01-starting-setup/src/App.vue
+++ animations-01-starting-setup/src/App.vue
@@ -4,7 +4,8 @@
     <button @click="animateBlock">Animate</button>
   </div>
   <div class="container">
-    <transition>
+    <!-- <transition enter-to-class="some-class" enter-active-class="..."> -->
+    <transition name="para">
       <p v-if="paraisVisible">This is only sometimes visible...</p>
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
@@ -91,32 +92,33 @@ button:active {
   animation: slide-scale 0.3s ease-out forwards;
 }
 
-.v-enter-from {
+.para-enter-from {
+  /* v 자리에 name값으로 치환 v->para */
   /* opacity: 0;
   transform: translateY(-30px); */
 }
 
-.v-enter-active {
+.para-enter-active {
   /* transition: all 0.3s ease-out; */
   animation: slide-scale 0.3s ease-out;
 }
 
-.v-enter-to {
+.para-enter-to {
   /* opacity: 1;
   transform: translateY(0); */
 }
 
-.v-leave-from {
+.para-leave-from {
   /* opacity: 1;
   transform: translateY(0); */
 }
 
-.v-leave-active {
+.para-leave-active {
   /* transition: all 0.3s ease-in; */
   animation: slide-scale 0.3s ease-out;
 }
 
-.v-leave-to {
+.para-leave-to {
   /* opacity: 0;
   transform: translateY(30px); */
 }
