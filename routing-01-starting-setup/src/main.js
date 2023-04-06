fbab72da5a221c2c064406c840e0ee784461ab99
kimjunhee <developer9101@gmail.com>
2023-04-06 11:42:25 +0900

    router 여러개 사용(components)

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -6,17 +6,19 @@ import TeamsList from './components/teams/TeamsList.vue'
 import UsersList from './components/users/UsersList.vue'
 import TeamMembers from './components/teams/TeamMembers.vue'
 import NotFound from './components/nav/NotFound.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue'
+import UsersFooter from './components/users/UsersFooter.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/teams' },
     {
-      name: 'teams', path: '/teams', component: TeamsList, children: [
+      name: 'teams', path: '/teams', components: { default: TeamsList, footer: TeamsFooter }, children: [
         { name: 'team-members', path: ':teamId', component: TeamMembers, props: true }, // /teams/:teamId
       ]
     }, // , alias: '/'
-    { path: '/users', component: UsersList },
+    { path: '/users', components: { default: UsersList, footer: UsersFooter } },
     { path: '/:notFound(.*)', component: NotFound }//redirect: '/teams' 
   ],
   // linkActiveClass: 'active' // active class name 변경
