7201b64f0af0c4554296509c82ef1bf6d5fb4985
kimjunhee <developer9101@gmail.com>
2023-04-07 23:00:10 +0900

    코치 등록 양식 화면 추가

--- 01-main-prj-starting-setup/src/pages/choaches/CoachRegistraction.vue
+++ 01-main-prj-starting-setup/src/pages/choaches/CoachRegistraction.vue
@@ -1,3 +1,15 @@
 <template>
-  <section>CoachRegistraction</section>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm';
+export default {
+  components: { CoachForm },
+};
+</script>
