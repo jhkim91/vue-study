b2b13f8ae38e1f62b356ce630561e150a8aa0f04
kimjunhee <developer9101@gmail.com>
2023-04-07 15:06:28 +0900

    route 설정 및 등록

---
+++ 01-main-prj-starting-setup/src/router.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import ChachDetail from './pages/choaches/ChachDetail.vue'
+import CoachesList from './pages/choaches/CoachesList.vue'
+import CoachRegistraction from './pages/choaches/CoachRegistraction.vue'
+import ContactCoach from './pages/requests/ContactCoach.vue'
+import RequestsReceived from './pages/requests/RequestsReceived.vue'
+import NotFound from './pages/NotFound.vue'
+
+const router = createRouter({
+  history: createWebHistory,
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches:id',
+      component: ChachDetail,
+      children: [
+        { path: 'contact', component: ContactCoach }, // /coaches/contact
+      ]
+    },
+    { path: '/register', component: CoachRegistraction },
+    { path: '/requests', component: RequestsReceived },
+    { path: '/:notFount(.*)', component: NotFound },
+  ]
+})
+
+export default router
