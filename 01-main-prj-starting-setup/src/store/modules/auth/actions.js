5f8a795d5d507f8b441107d0be7e0bed77e61f3a
kimjunhee <developer9101@gmail.com>
2023-04-08 22:28:54 +0900

    회원가입 액션 및 플로우 추가

--- 01-main-prj-starting-setup/src/store/modules/auth/actions.js
+++ 01-main-prj-starting-setup/src/store/modules/auth/actions.js
@@ -1,1 +1,29 @@
-export default {};
+export default {
+  login() { },
+  async signup(context, payload) {
+    // firebase docs: https://firebase.google.com/docs/reference/rest/auth?hl=ko#section-create-email-password
+    const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDnm15j6-90riVljWUfRDPlSAhy0u5SlCg`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true
+      })
+    })
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData)
+      const error = new Error(responseData.message || 'Failed to authenticate.')
+      throw error
+    }
+
+    console.log(responseData)
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    })
+  }
+};
