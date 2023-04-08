9faa2abe879c8eaf2a624c67c5cfb85918d6e8bc
kimjunhee <developer9101@gmail.com>
2023-04-08 22:05:20 +0900

    인증 페이지(로그인 및 회원가입) 추가

--- 01-main-prj-starting-setup/src/router.js
+++ 01-main-prj-starting-setup/src/router.js
@@ -5,6 +5,7 @@ import CoachesList from './pages/choaches/CoachesList.vue'
 import CoachRegistraction from './pages/choaches/CoachRegistraction.vue'
 import ContactCoach from './pages/requests/ContactCoach.vue'
 import RequestsReceived from './pages/requests/RequestsReceived.vue'
+import UserAuth from './pages/auth/UserAuth.vue'
 import NotFound from './pages/NotFound.vue'
 
 const router = createRouter({
@@ -22,6 +23,7 @@ const router = createRouter({
     },
     { path: '/register', component: CoachRegistraction },
     { path: '/requests', component: RequestsReceived },
+    { path: '/auth', component: UserAuth },
     { path: '/:notFount(.*)', component: NotFound },
   ]
 })
