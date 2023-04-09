bead39eb883725089e2794c1250ba492b70d1ab8
kimjunhee <developer9101@gmail.com>
2023-04-09 10:33:43 +0900

    최적화: 비동기 컴포넌트 사용하기

--- 01-main-prj-starting-setup/src/router.js
+++ 01-main-prj-starting-setup/src/router.js
@@ -1,13 +1,20 @@
+import { defineAsyncComponent } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetail from './pages/choaches/CoachDetail.vue'
+// import CoachDetail from './pages/choaches/CoachDetail.vue'
 import CoachesList from './pages/choaches/CoachesList.vue'
-import CoachRegistraction from './pages/choaches/CoachRegistraction.vue'
-import ContactCoach from './pages/requests/ContactCoach.vue'
-import RequestsReceived from './pages/requests/RequestsReceived.vue'
-import UserAuth from './pages/auth/UserAuth.vue'
+// import CoachRegistraction from './pages/choaches/CoachRegistraction.vue'
+// import ContactCoach from './pages/requests/ContactCoach.vue'
+// import RequestsReceived from './pages/requests/RequestsReceived.vue'
+// import UserAuth from './pages/auth/UserAuth.vue'
 import NotFound from './pages/NotFound.vue'
 import store from './store/index.js' // router 에서 store 값 참조
+
+const CoachDetail = defineAsyncComponent(() => import('./pages/choaches/CoachDetail.vue'))
+const CoachRegistraction = defineAsyncComponent(() => import('./pages/choaches/CoachRegistraction.vue'))
+const ContactCoach = defineAsyncComponent(() => import('./pages/requests/ContactCoach.vue'))
+const RequestsReceived = defineAsyncComponent(() => import('./pages/requests/RequestsReceived.vue'))
+const UserAuth = defineAsyncComponent(() => import('./pages/auth/UserAuth.vue'))
 
 const router = createRouter({
   history: createWebHistory(),
