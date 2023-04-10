cb28a0e7a09df93fad42b02d7e04137d7d08de98
kimjunhee <developer9101@gmail.com>
2023-04-10 12:45:34 +0900

    setup() "반응형" 객체 구축

--- 02-composition-starting-setup/src/App.vue
+++ 02-composition-starting-setup/src/App.vue
@@ -1,23 +1,42 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
+    <h2>{{ userRefs.name }}</h2>
+    <h3>{{ user.age }}</h3>
   </section>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, reactive, toRefs, isRef, isReactive } from 'vue';
 
 export default {
   setup() {
     // ref 사용시 반응형 값이 됨
-    let uName = ref('Maximilian');
+
+    const uName = ref('Maximilian');
+    // => uName.value 로 수정, 요소에서 사용시 uName으로 사용
+
+    // object 사용시 object를 넘겨야함 이유: 요소에서 user.value.name 값은 ref에서 추출한 값이기때문
+    // const user = ref({ name: 'Maximilian', age: 31, });
+
+    // 그래서 object 는 reactive 사용 (user.name 으로 액세스 가능)
+    const user = reactive({ name: 'Maximilian', age: 31 });
+    // reactive 내부값을 모두 ref 치환
+    const userRefs = toRefs(user);
+
+    console.log(isRef(uName));
+    console.log(isReactive(user));
 
     setTimeout(() => {
-      uName.value = 'Max';
+      user.name = 'Max';
+      user.age = 32;
     }, 2000);
 
     return {
-      userName: uName,
+      uName,
+      userName: user.name,
+      age: user.age,
+      user: user,
+      userRefs,
     };
   },
   // data() {
