203eed15eaae27e9ec84c6e0081567cab82be308
kimjunhee <developer9101@gmail.com>
2023-04-10 21:48:08 +0900

    router 객체 및 컴포지션 api

--- composition-17-routing-starting-project/src/pages/ProductDetails.vue
+++ composition-17-routing-starting-project/src/pages/ProductDetails.vue
@@ -9,13 +9,16 @@
 
 <script>
 import { inject, computed } from 'vue';
+import { useRoute } from 'vue-router';
 
 export default {
   props: ['pid'],
-  setup(props) {
+  setup() {
     const products = inject('products');
+    const route = useRoute();
+    console.log(route.params.pid);
     const selectedProduct = computed(() =>
-      products.value.find((product) => product.id === props.pid)
+      products.value.find((product) => product.id === route.params.pid)
     );
     const title = computed(() => selectedProduct.value.title);
     const price = computed(() => selectedProduct.value.price);
